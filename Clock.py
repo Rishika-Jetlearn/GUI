5b0b317bd1d7f8fc93fb5ffc86ee55b4643f98ac
rishviltd <info@rishvi.co.uk>
2024-10-11 19:56:21 +0100

    update

---
+++ Clock.py
@@ -0,0 +1,12 @@
+from tkinter import*
+from time import strftime
+def time():
+    clock=strftime("%H:%M:%S")
+    label.config(text=clock)
+    label.after(1000,time)
+window=Tk()
+window.geometry("500x200")
+label=Label(window,foreground="black",background="lightyellow",font=("comicsans",24,"bold"))
+label.pack()
+time()
+window.mainloop()
