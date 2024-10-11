d5fbe5d1a1d7ce0cd8de353be260e4e1f409892c
rishviltd <info@rishvi.co.uk>
2024-10-11 19:31:19 +0100

    update

--- Weight converter.py
+++ Weight converter.py
@@ -1,23 +1,20 @@
 from tkinter import*
+
+#coverting
+def convert():
+    kg=float(ent.get())
+    ans1=kg*1000
+
+    ans2=kg*2.20462
+
+    ans3=kg*35.274
+
+    gra.config(text=ans1)
+    pou.config(text=ans2)
+    oun.config(text=ans3)
 window=Tk()
 window.geometry("300x400")
 window.title("Weight converter")
-
-
-#coverting to grams
-def covert_gram():
-    gr=float(ent.get())
-    ans1=gr*1000
-
-#coverting to pounds
-def covert_pounds():
-    po=float(ent.get)
-    ans2=po*2.20462
-
-#coverting to ounce
-def convert_ounce():
-    ou=float(ent.get)
-    ans3=ou*35.274
 
 
 #label
@@ -25,24 +22,33 @@ lab=Label(window,text="Enter weight in kg")
 lab.grid(row=0,column=0)
 #entry
 ent=Entry(window)
-ent.grid(row=1,column=0)
+ent.grid(row=0,column=1)
 
 #button
-conv=Button(window,text="convert")
-conv.grid(row=2,column=0)
+conv=Button(window,text="convert",command=convert)
+conv.grid(row=1,column=1)
 
 #gram
 g=Label(window,text="gram")
-lab.grid(row=1,column=0)
+g.grid(row=2,column=0)
 
 #pounds
 p=Label(window,text="pounds")
-lab.grid(row=1,column=1)
+p.grid(row=2,column=1)
 
 #ounce
 o=Label(window,text="ounce")
-lab.grid(row=1,column=2)
+o.grid(row=2,column=2)
 
+#ans1
+gra=Label(window)
+gra.grid(row=3,column=0)
+#ans2
+pou=Label(window)
+pou.grid(row=3,column=1)
+#ans3
+oun=Label(window)
+oun.grid(row=3,column=2)
 
 
 
