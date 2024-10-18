886c743fbc54c6fcc5e60a57684eb9cc063c2903
rishviltd <info@rishvi.co.uk>
2024-10-18 20:01:11 +0100

    update

--- rock,paper,sissors h.w .py
+++ rock,paper,sissors h.w .py
@@ -1,7 +1,24 @@
 from tkinter import*
 import random
+options=["rock","paper","sissors"]
+com=0
+ply=0
+def logic(player):
+    global com,ply
+    computer=random.choice(options)
+    if computer==player:
+        result.config(text="Tie")
+    elif (computer==rock and player==sissors) or (computer==paper and player==rock) or (computer==sissors and player==paper):
+        result.config(text="Computer wins!")
+        com=com+1
+    else:
+        result.config(text="You win!")
+        ply=ply+1
 
-def roc():
+    youselec.config(text=f"You selected: {player}")
+    comselec.config(text=f"Computer selected: {computer}")
+    playersc.config(f"Player score {str(ply)}")
+    comsc.config(f"computer score {str(com)}")
 
 window=Tk()
 window.geometry("300x400")
@@ -11,17 +28,20 @@ window.title("Rock,paper,sissors")
 lab=Label(window,text="Rock,Paper,Sissors",foreground="grey",font="Comicsans")
 lab.grid(row=1,column=3)
 
+result=Label(window,text="You vs. Computer")
+result.grid(row=2,column=3)
+
 your_options=Label(window,text="Your options")
 your_options.grid(row=2,column=2)
 
 #buttons
-rock=Button(text="rock")
+rock=Button(text="rock",command=lambda: logic(options[0]))
 rock.grid(row=3,column=3)
 
-paper=Button(text="paper")
+paper=Button(text="paper",command=lambda: logic(options[1]))
 paper.grid(row=4,column=3)
 
-sissors=Button(text="sissors")
+sissors=Button(text="sissors",command=lambda:logic(options[2]))
 sissors.grid(row=5,column=3)
 
 #score
